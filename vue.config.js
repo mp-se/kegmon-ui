262158506a1dbbf05a4981bfa4f69d1601580cea
Magnus <mp-se@users.noreply.github.com>
2024-07-28 09:13:38 +0200

    Included chartjs in build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 const CompressionPlugin = require("compression-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -17,13 +16,9 @@ module.exports = defineConfig({
       headers: { "Access-Control-Allow-Origin": "*" }
     },
     plugins: [
-      // new BundleAnalyzerPlugin(),
       new CompressionPlugin({
         algorithm: "gzip",
       }),
-    ],
-    externals: {
-      'chart.js': 'Chart'
-    }
+    ]
   },
 })
