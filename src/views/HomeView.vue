0ff6c4153469ed7b3c59c1a3a786164b56bb32a1
Magnus <mp-se@noreply.github.com>
2024-12-11 18:55:16 +0100

    Adding ID to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -126,7 +126,7 @@
         <div class="col-md-4">
           <BsCard header="Device" title="Platform">
             <p class="text-center">
-              {{ status.platform }}
+              {{ status.platform }} (ID: {{ status.id }})
             </p>
           </BsCard>
         </div>
